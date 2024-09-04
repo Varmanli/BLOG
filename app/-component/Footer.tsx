fc4077aad7424aa7848bc92e245828c42ef3ec96
varmanli <varmanliamirhosein@gmail.com>
2024-09-04 04:08:44 +0330

    fix commit

--- app/-component/Footer.tsx
+++ app/-component/Footer.tsx
@@ -2,8 +2,11 @@ import { FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className="flex items-center justify-center gap-2 py-2 md:p-10 text-lg bg-[#400068] text-white/80 ">
-      <FaGithub className="mb-1 text-2xl cursor-pointer " />
+    <div className="flex items-center justify-center gap-2 py-10 md:p-10 text-lg bg-[#400068] text-white/80 ">
+      <FaGithub
+        className="mb-1 text-2xl cursor-pointer "
+        href="https://github.com/Varmanli/BLOG?tab=readme-ov-file#readme"
+      />
       <h1>Development By Amirhosein Varmanli</h1>
     </div>
   );

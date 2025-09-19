120747d6e547635400cbe03eafdae768afd04d25
varmanli <varmanliamirhosein@gmail.com>
2025-09-20 02:00:55 +0330

    fix:ui blog section

--- app/-component/BlogSection.tsx
+++ app/-component/BlogSection.tsx
@@ -263,7 +263,7 @@ export default function BlogSection({
       {/* لیست بلاگ‌ها */}
       {displayBlogs.length ? (
         <motion.div
-          className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 z-10 relative"
+          className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 z-10 relative"
           variants={containerVariants}
           initial="hidden"
           animate="visible"
